a5ed819a9dc6300245dc5c3e8c420249e664e952
lichader <1524470+lichader@users.noreply.github.com>
2025-08-10 14:25:28 +1000

    version 2 of binary search

--- src/main/java/algorithm/BinarySearch.java
+++ src/main/java/algorithm/BinarySearch.java
@@ -3,33 +3,22 @@ package algorithm;
 public class BinarySearch {
 
     public int indexOf(int[] input, int target) {
-        if (input.length == 0) {
-            return -1;
+        int start = 0;
+        int end = input.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (input[mid] == target) {
+                return mid;
+            } else if (input[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
         }
 
-        return search(input, 0, input.length - 1, target);
-    }
-
-    private int search(int[] input, int start, int end, int target) {
-        if (start == end && input[start] == target) {
-            return start;
-        } else if (start < 0 || end < 0 || start >= input.length || end >= input.length) {
-            return -1;
-        }
-
-        int middle = start + (end - start) / 2;
-
-        int newStart = start;
-        int newEnd = end;
-
-        if (input[middle] < target) {
-            newStart = middle + 1;
-        } else if (input[middle] > target) {
-            newEnd = middle - 1;
-        } else {
-            return middle;
-        }
-
-        return search(input, newStart, newEnd, target);
+        return -1;
     }
 }
